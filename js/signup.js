667a7d4bfe8ed2a8ef1cd3aaa5681ecdcb8fc69c
miguelrodrica <mangelrodca@gmail.com>
2026-02-02 15:36:43 -0500

    Funcionalidad JavaScript del Sign Up

---
+++ js/signup.js
@@ -0,0 +1,48 @@
+const form = document.querySelector('#signupForm');
+const nameUser = document.querySelector('#nameInputSignup');
+const emailUser = document.querySelector('#emailInputSignup');
+const passwordUser = document.querySelector('#passwordInputSignup');
+const confirmPasswordUser = document.querySelector('#confirmPasswordInputSignup');
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const response = await fetch('http://localhost:3000/users');
+    const users = await response.json();
+    let emailExists = false;
+
+    for (let user of users) {
+        if (user.email === emailUser.value) {
+            emailExists = true;
+            break;
+        }
+    };
+
+    if (emailExists) {
+        alert('Este email ya está registrado');
+        return;
+    };
+
+    if (passwordUser.value !== confirmPasswordUser.value) {
+        alert('Las contraseñas no coinciden');
+        return;
+    }
+    
+    const newUser = {
+        name: nameUser.value,
+        email: emailUser.value,
+        password: passwordUser.value,
+        role: "user"
+    };
+
+    await fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newUser)
+    });
+
+    alert('Usuario registrado correctamente');
+    form.reset();
+});
